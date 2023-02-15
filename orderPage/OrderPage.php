1028d6842ab22d9692c27eb75c5fe9b28e692925
bukangliwayway <jamesmatthewbelgica@gmail.com>
2023-02-16 06:45:25 +0800

    Main Order Page

---
+++ orderPage/OrderPage.php
@@ -0,0 +1,95 @@
+<?php
+
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "JuanCafe";
+
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+  $sql1 = "SELECT * FROM Beverage";
+  $result1 = $conn->query($sql1);
+
+  $beverages = array();
+
+  if ($result1->num_rows > 0) {
+    while ($row = $result1->fetch_assoc()) {
+      $beverage = new stdClass();
+      $beverage->id = $row['BeverageID'];
+      $beverage->name = $row['BeverageName'];
+      $beverage->image = $row['BeverageImagePath'];
+      $beverage->price = $row['BeveragePrice'];
+      $beverage->size = $row['BeverageSize'];
+      $beverage->category = $row['BeverageCategory'];
+      $beverages[] = $beverage;
+    }
+  }
+
+  $sql2 = "SELECT DISTINCT BeverageCategory FROM Beverage;";
+  $result2 = $conn->query($sql2);
+
+  $categories = array();
+
+  if ($result1->num_rows > 0) 
+    while ($row = $result2->fetch_assoc()) 
+      $categories[] = $row['BeverageCategory'];
+    
+
+  $conn->close();
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <script defer src="main.js"></script>
+  </head>
+  <body>
+    <div class="coffee-bg">
+      <div class="greetings">
+        <h1>Hi, Kath</h1>
+        <h5>What's your Favorite Drink</h5>
+      </div>
+      <div class="cart">
+        <img src="#" alt="cart" />
+      </div>
+    </div>
+    <div class="upper">
+      <h2>Categories</h2>
+      <div class="sections">
+        <?php
+        foreach($categories as $category){
+          echo'<div class="category"><img src="#" alt="icon" /><h2>'.$category.'</h2>
+            </div>';
+        }
+        ?>
+      </div>
+    </div>
+    <div class="beverages">
+      <?php
+        foreach($beverages as $beverage){
+          echo '<div class="card" data-category="'.$beverage->category.'">
+                  <img src="'.$beverage->image.'" alt="'.$beverage->name.'" />
+                  <div class="beverage-info">
+                    <h2 class="beverage-title">'.$beverage->name.'</h2>
+                    <h2 class="beverage-price">'.$beverage->price.'</h2>
+                  </div>
+                  <a href="#"><img src="#" alt="add-icon" /></a>
+                </div>';
+        }
+      ?>
+    </div>
+    <div class="coffee-bg">
+      <h3>Items</h3>
+      <span id="item">0</span>
+      <h3>Total Price</h3>
+      <span id="total-price">P 0</span>
+    </div>
+  </body>
+</html>
--- orderPage/save_to_cart.php
+++
@@ -1,6 +0,0 @@
-<?php
-  $item = $_POST["item"];
-  $file = fopen("selected.txt", "a");
-  fwrite($file, $item . "\n");
-  fclose($file);
-?>
